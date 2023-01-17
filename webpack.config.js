420a15d5271bcf7dcf72a3c45fca16a4d1748e9c
JaimePerales <jaimeperales123@gmail.com>
2023-01-16 18:14:25 -0600

    Correct boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
     mode: 'development',
@@ -10,19 +10,11 @@ module.exports = {
         static: './dist',
 
     },
-    plugins: [
 
-        new HtmlWebpackPlugin({
-
-            title: 'Development',
-
-        }),
-
-    ],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true
+
     },
 
     module: {
